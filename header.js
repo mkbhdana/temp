e24dfffdd10843ff8b10a1cb2da6fd89b928d6f7
Mayank Bhadana <mkbhdana@gmail.com>
2022-08-30 18:45:45 +0530

    Update header.js

--- header.js
+++ header.js
@@ -9,7 +9,7 @@ function Header() {
         <h1 style={{ fontWeight: 'bold', color: 'steelblue' }}>Keyboard Test</h1>
         <p style={{ color: 'gray', paddingBottom: '30px' }}>Free online keyboard test to check if the keyboard keys are working well or not.</p>
         <textarea
-          rows="10"
+          rows="7"
           cols="130"
           placeholder="Press any key...."
           style={{
