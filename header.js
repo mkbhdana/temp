c1630e53fbfba2dbc7cf751fa40726336430c587
Mayank Bhadana <mkbhdana@gmail.com>
2022-08-30 18:39:25 +0530

    Update header.js

--- header.js
+++ header.js
@@ -7,16 +7,14 @@ function Header() {
 
       <div className="container">
         <h1 style={{ fontWeight: 'bold', color: 'steelblue' }}>Keyboard Test</h1>
-        <p style={{ color: 'gray' }}>
-          Free online keyboard test to check if the keyboard keys are working well
-          or not.
-        </p>
+        <p style={{ color: 'gray', paddingBottom: '30px' }}>Free online keyboard test to check if the keyboard keys are working well or not.</p>
         <textarea
-          className="form-control"
-          rows="7"
-          cols="80"
+          rows="10"
+          cols="130"
           placeholder="Press any key...."
-          style={{ maxWidth: '95%' }}
+          style={{
+            maxWidth: '95%', border: '4px solid white', borderRadius: '8px'
+          }}
         ></textarea>
       </div>
     </div >
