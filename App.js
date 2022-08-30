4afb6545d0c80d237b40907ed23867efdab72256
Mayank Bhadana <mkbhdana@gmail.com>
2022-08-30 16:17:15 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import Header from "./components/header";
+import Keyboard from "./components/keyboard";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Keyboard />
+    </div>
+  );
+}
+
+export default App;
